259789a2a2e174f1511a6cf0342c83e6b8a5411b
Luke Sellmayer <luse9638@colorado.edu>
2022-12-22 20:05:33 -0700

    More detailed hitboxes and all collisions between different sprites are now identified

--- sim.py
+++ sim.py
@@ -4,9 +4,9 @@ import random
 # extend the Sprite class for the rock, paper, and scissor icons
 class Weapon(arcade.Sprite):
     # constructor
-    def __init__(self, image, scaling):
+    def __init__(self, filename, scale, hit_box_algorithm):
         # call the Sprite class's constructor
-        super().__init__(image, scaling)
+        super().__init__(filename = filename, scale = scale, hit_box_algorithm = hit_box_algorithm)
 
     def update(self):
         pass
@@ -44,7 +44,7 @@ class Game(arcade.Window):
         # SPRITES ARE 120 X 120 PIXELS
         for i in range(33):
             # rock sprites
-            rock = Weapon("Sprites/rock.png", 0.25)
+            rock = Weapon(filename = "Sprites/rock.png", scale = 0.25, hit_box_algorithm = "Detailed")
             # set position on screen to be random, adjust so the entire sprite is on the screen
             rock.center_x = random.randrange(60, Game.screenWidth - 60)
             rock.center_y = random.randrange(60, Game.screenHeight - 60)
@@ -52,7 +52,7 @@ class Game(arcade.Window):
             self.rockList.append(rock)
 
             # paper sprites
-            paper = Weapon("Sprites/paper.png", 0.25)
+            paper = Weapon(filename = "Sprites/paper.png", scale = 0.25, hit_box_algorithm = "Detailed")
             # set position on screen to be random
             paper.center_x = random.randrange(60, Game.screenWidth - 60)
             paper.center_y = random.randrange(60, Game.screenHeight - 60)
@@ -60,7 +60,7 @@ class Game(arcade.Window):
             self.paperList.append(paper)
 
             # scissor sprites
-            scissor = Weapon("Sprites/scissors.png", 0.25)
+            scissor = Weapon(filename = "Sprites/scissors.png", scale = 0.25, hit_box_algorithm = "Detailed")
             # set position on screen to be random
             scissor.center_x = random.randrange(60, Game.screenWidth - 60)
             scissor.center_y = random.randrange(60, Game.screenHeight - 60)
@@ -68,12 +68,20 @@ class Game(arcade.Window):
             self.scissorList.append(scissor)
 
         # check for initial collisions
-        # iterate over every rock
+        # rock and paper collisions
         for rock in self.rockList:
-            # check if rock intersects with any of the papers
-            for list in arcade.check_for_collision_with_list(rock, self.paperList):
-                # add coordinates of rock to list
+            for collision in arcade.check_for_collision_with_list(rock, self.paperList):
                 Game.collisionList.append((rock.center_x, rock.center_y))
+
+        # rock and scissor collisions
+        for rock in self.rockList:
+            for collision in arcade.check_for_collision_with_list(rock, self.scissorList):
+                Game.collisionList.append((rock.center_x, rock.center_y))
+
+        # paper and scissor collisions
+        for paper in self.paperList:
+            for collision in arcade.check_for_collision_with_list(paper, self.scissorList):
+                Game.collisionList.append((paper.center_x, paper.center_y))
 
     # draws things on screen 60 times a second
     def on_draw(self):
