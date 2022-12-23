cf23d3b22481a884030419393eda7da93627c77b
Luke Sellmayer <luse9638@colorado.edu>
2022-12-22 19:34:14 -0700

    Randomly show rocks, papers, scissorsss on screen

--- sim.py
+++ sim.py
@@ -1,24 +1,88 @@
 import arcade
+import random
+
+# extend the Sprite class for the rock, paper, and scissor icons
+class Weapon(arcade.Sprite):
+    # constructor
+    def __init__(self, image, scaling):
+        # call the Sprite class's constructor
+        super().__init__(image, scaling)
+
+    def update(self):
+        pass
 
 # extend arcade's built in Window class
 class Game(arcade.Window):
+    # static variables to store screen's native width and height
+    screenWidth = arcade.get_display_size()[0]
+    screenHeight = arcade.get_display_size()[1]
+    
     # constructor
-    def __init__(self, width, height, title):
+    def __init__(self):
         # call the Window class's constructor
-        super().__init__(width, height, title)
+        super().__init__(Game.screenWidth, Game.screenHeight, "Example")
+
+        # declare the sprite lists
+        self.rockList = None
+        self.paperList = None
+        self.scissorList = None
 
         # set background color
         arcade.set_background_color(arcade.color.WHITE)
+
+    # sets up the game, can be called multiple times to restart the game
+    def setup(self):
+        # initialize the sprite lists using arcade's SpriteList
+        self.rockList = arcade.SpriteList()
+        self.paperList = arcade.SpriteList()
+        self.scissorList = arcade.SpriteList()
+
+        # create all of the sprites
+        # SPRITES ARE 120 X 120 PIXELS
+        for i in range(33):
+            # rock sprites
+            rock = Weapon("Sprites/rock.png", 0.25)
+            # set position on screen to be random
+            rock.center_x = random.randrange(60, Game.screenWidth - 60)
+            rock.center_y = random.randrange(60, Game.screenHeight - 60)
+            # add to sprite list
+            self.rockList.append(rock)
+
+            # paper sprites
+            paper = Weapon("Sprites/paper.png", 0.25)
+            # set position on screen to be random
+            paper.center_x = random.randrange(60, Game.screenWidth - 60)
+            paper.center_y = random.randrange(60, Game.screenHeight - 60)
+            # add to sprite list
+            self.paperList.append(paper)
+
+            # scissor sprites
+            scissor = Weapon("Sprites/scissors.png", 0.25)
+            # set position on screen to be random
+            scissor.center_x = random.randrange(60, Game.screenWidth - 60)
+            scissor.center_y = random.randrange(60, Game.screenHeight - 60)
+            # add to sprite list
+            self.scissorList.append(scissor)
 
     # draws things on screen 60 times a second
     def on_draw(self):
         # ready to start drawing
         arcade.start_render()
 
+        # draw all of the sprites
+        self.rockList.draw()
+        self.paperList.draw()
+        self.scissorList.draw()
+
+    # updates values 60 times a second
+    def update(self, delta_time):
+        pass
+
 def main():
     # create game window with with width, height, and name
-    window = Game(640, 480, "Drawing Example")
-    
+    window = Game()
+    # run game setup
+    window.setup()
     # keep window open until user closes it
     arcade.run()
 
